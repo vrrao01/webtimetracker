9b6344bdb980dcc0bff4e2ab785efb3c8245a5fe
vrrao01 <75874394+vrrao01@users.noreply.github.com>
2021-02-05 00:50:31 +0530

    Revert "Version1.2.0"

--- background.js
+++ background.js
@@ -2,47 +2,27 @@ function getDomain(tablink){
     let url =  tablink[0].url;
     return url.split("/")[2];
 };
-var timeSoFar;
+
 function updateTime(){
-        let nDate=new Date();
-        let nDateDate=nDate.getDate();
-        let nDateMonth=nDate.getMonth()+1;
-        let nDateYear=nDate.getFullYear().toString();
-        if(nDateDate<10){nDateDate="0"+nDateDate.toString();};
-        if(nDateMonth<10){nDateMonth="0"+nDateMonth.toString();};
-        let presentDate=nDateDate+nDateMonth+nDateYear;
     chrome.tabs.query({active:true},function(activeTab){
         let domain = getDomain(activeTab);
         let myObj = {};
-        myObj[presentDate]={};
-        myObj[presentDate][domain] = "";
-        chrome.storage.local.get(null,function(storedObject){
+        myObj[domain] = "";
+        chrome.storage.local.get(domain,function(storedObject){
             timeSoFar = 0;
-            if(storedObject[presentDate]){
-                if(storedObject[presentDate][domain]){
-                    timeSoFar = storedObject[presentDate][domain]+1;
-                    myObj = storedObject;
-                    myObj[presentDate][domain] = timeSoFar;
-                    chrome.storage.local.set(myObj,function(){
-                        console.log(`${myObj[presentDate][domain]} in ${domain}`);
-                    });
-                }
-                else{
-                    timeSoFar++;
-                    myObj = storedObject;
-                    myObj[presentDate][domain] = timeSoFar;
-                    chrome.storage.local.set(myObj,function(){
-                        console.log(`${myObj[presentDate][domain]} in ${domain}`);    
-                    });
-                }
+            if(storedObject[domain]){
+                timeSoFar = storedObject[domain]+1;
+                myObj[domain] = timeSoFar;
+                chrome.storage.local.set(myObj,function(){
+                    console.log("Set "+domain+" at "+myObj[domain]);
+                });
             }
             else{
                 timeSoFar++;
-                myObj = storedObject;
-                myObj[presentDate] = {};
-                myObj[presentDate][domain]= timeSoFar;
+                myObj[domain] = timeSoFar;
                 chrome.storage.local.set(myObj,function(){
-                console.log(`${myObj[presentDate][domain]} in ${domain}`);});
+                    console.log("Set "+domain+" at "+myObj[domain]);    
+                });
             }
         });
     });
--- popup.js
+++ popup.js
@@ -1,37 +1,32 @@
 chrome.storage.local.get(null,function(siteList){
-  var dateShown='04022021';
-  let allKeys = Object.keys(siteList[dateShown]);
-  let timeSpent = [];
-  let color=[];
-  let insI=document.getElementById('insInside');
-  let insB=document.getElementById('insBefore');
-    document.getElementById('btn').addEventListener("click",function(){
-      document.getElementById('frmDate').innerHTML='<input type="date" id="hidnDate"><input type="button" value="View by date" id="hidnButton">';
-      document.getElementById('hidnButton').addEventListener('click',function(){
-        var selectedDate=document.getElementById('hidnDate').value;
-        selectedDate=selectedDate.split('-');
-        selectedDate=selectedDate[2]+selectedDate[1]+selectedDate[0];
-        dateShown=selectedDate.toString();
-        allKeys = Object.keys(siteList[dateShown]);
-      })
-      for(let i = 0; i<allKeys.length;i++){
-          let webURL = allKeys[i];
-          let nDA=document.createElement('tr');
-          let nDB=document.createElement('td');
-          let nDC=document.createTextNode(webURL);
-          let nDD=document.createElement('td');
-          let nDE=document.createTextNode(siteList[dateShown][webURL]+"sec");
-          nDA.appendChild(nDB).appendChild(nDC);
-          nDA.appendChild(nDD).appendChild(nDE);
-          insI.insertBefore(nDA,insB);
-        }
-        document.getElementById('btn').setAttribute('disabled',true);
-      })
+  allKeys = Object.keys(siteList);
+  timeSpent = [];
+  color=[];
+  insI=document.getElementById('insInside');
+  insB=document.getElementById('insBefore');
   for (let i = 0; i<allKeys.length;i++ ){
     let webURL = allKeys[i];
-    timeSpent.push(siteList[dateShown][webURL]);
+    timeSpent.push(siteList[webURL]);
     color.push('#'+Math.floor(Math.random()*16777215).toString(16));
+    //console.log(webURL);
     }
+    document.getElementById('btn').addEventListener("click",function(){
+    for(let i = 0; i<allKeys.length;i++){
+        let webURL = allKeys[i];
+        let nDA=document.createElement('tr');
+        let nDB=document.createElement('td');
+        let nDC=document.createTextNode(webURL);
+        let nDD=document.createElement('td');
+        let nDE=document.createTextNode(siteList[webURL]+"sec");
+        nDA.appendChild(nDB).appendChild(nDC);
+        nDA.appendChild(nDD).appendChild(nDE);
+        insI.insertBefore(nDA,insB);
+        console.log(webURL)
+    }
+    })
+});
+
+setTimeout(function(){
   new Chart(document.getElementById("pie-chart"), {
     type: 'pie',
     data: {
@@ -50,7 +45,8 @@ chrome.storage.local.get(null,function(siteList){
           display:false
       }
     }
-  });
 });
+},10)
+
 
 
