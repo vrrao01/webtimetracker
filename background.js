ad9ad94d76dc8477f9d15ef5004e48f3a70e00d0
Shankar <gowrishankarirwog@gmail.com>
2021-02-04 20:05:08 +0530

    version1.2.0

--- background.js
+++ background.js
@@ -2,27 +2,47 @@ function getDomain(tablink){
     let url =  tablink[0].url;
     return url.split("/")[2];
 };
-
+var timeSoFar;
 function updateTime(){
+        let nDate=new Date();
+        let nDateDate=nDate.getDate();
+        let nDateMonth=nDate.getMonth()+1;
+        let nDateYear=nDate.getFullYear().toString();
+        if(nDateDate<10){nDateDate="0"+nDateDate.toString();};
+        if(nDateMonth<10){nDateMonth="0"+nDateMonth.toString();};
+        let presentDate=nDateDate+nDateMonth+nDateYear;
     chrome.tabs.query({active:true},function(activeTab){
         let domain = getDomain(activeTab);
         let myObj = {};
-        myObj[domain] = "";
-        chrome.storage.local.get(domain,function(storedObject){
+        myObj[presentDate]={};
+        myObj[presentDate][domain] = "";
+        chrome.storage.local.get(null,function(storedObject){
             timeSoFar = 0;
-            if(storedObject[domain]){
-                timeSoFar = storedObject[domain]+1;
-                myObj[domain] = timeSoFar;
-                chrome.storage.local.set(myObj,function(){
-                    console.log("Set "+domain+" at "+myObj[domain]);
-                });
+            if(storedObject[presentDate]){
+                if(storedObject[presentDate][domain]){
+                    timeSoFar = storedObject[presentDate][domain]+1;
+                    myObj = storedObject;
+                    myObj[presentDate][domain] = timeSoFar;
+                    chrome.storage.local.set(myObj,function(){
+                        console.log(`${myObj[presentDate][domain]} in ${domain}`);
+                    });
+                }
+                else{
+                    timeSoFar++;
+                    myObj = storedObject;
+                    myObj[presentDate][domain] = timeSoFar;
+                    chrome.storage.local.set(myObj,function(){
+                        console.log(`${myObj[presentDate][domain]} in ${domain}`);    
+                    });
+                }
             }
             else{
                 timeSoFar++;
-                myObj[domain] = timeSoFar;
+                myObj = storedObject;
+                myObj[presentDate] = {};
+                myObj[presentDate][domain]= timeSoFar;
                 chrome.storage.local.set(myObj,function(){
-                    console.log("Set "+domain+" at "+myObj[domain]);    
-                });
+                console.log(`${myObj[presentDate][domain]} in ${domain}`);});
             }
         });
     });
