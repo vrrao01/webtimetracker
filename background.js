04bfff20a505bfd1c1e38214595e37fe2870d989
vrrao <vigneshravirao@gmail.com>
2021-02-05 00:34:17 +0530

    Improved functionality

--- background.js
+++ background.js
@@ -1,3 +1,34 @@
+function secondsToString(seconds,compressed=false){
+    let hours = parseInt(seconds/3600);
+    seconds = seconds%3600;
+    let minutes= parseInt(seconds/60);
+    seconds = seconds%60;
+    let timeString = "";
+    if(hours){
+      timeString += hours + " hrs ";
+    }
+    if(minutes){
+      timeString += minutes + " min ";
+    }
+    if(seconds){
+      timeString += seconds+ " sec "
+    }
+    if(!compressed){
+      return timeString;
+    }
+    else{
+      if(hours){
+        return(`${hours}h`);
+      }
+      if(minutes){
+        return(`${minutes}m`);
+      }
+      if(seconds){
+        return(`${seconds}s`);
+      }
+    }
+  };
+
 function getDomain(tablink){
     let url =  tablink[0].url;
     return url.split("/")[2];
@@ -7,26 +38,29 @@ function updateTime(){
     chrome.tabs.query({active:true},function(activeTab){
         let domain = getDomain(activeTab);
         let myObj = {};
+        let timeSoFar = 0;
         myObj[domain] = "";
         chrome.storage.local.get(domain,function(storedObject){
-            timeSoFar = 0;
             if(storedObject[domain]){
                 timeSoFar = storedObject[domain]+1;
                 myObj[domain] = timeSoFar;
                 chrome.storage.local.set(myObj,function(){
                     console.log("Set "+domain+" at "+myObj[domain]);
+                    chrome.browserAction.setBadgeText({'text':secondsToString(timeSoFar,true)});
                 });
             }
             else{
                 timeSoFar++;
                 myObj[domain] = timeSoFar;
                 chrome.storage.local.set(myObj,function(){
-                    console.log("Set "+domain+" at "+myObj[domain]);    
+                    console.log("Set "+domain+" at "+myObj[domain]);  
+                    chrome.browserAction.setBadgeText({'text':secondsToString(timeSoFar,true)});  
                 });
             }
         });
     });
-    chrome.browserAction.setBadgeText({'text':timeSoFar.toString()});
+
+    // console.log(timeSoFar);
 };
 
 
--- popup.js
+++ popup.js
@@ -1,54 +1,102 @@
+function secondsToString(seconds,compressed=false){
+  let hours = parseInt(seconds/3600);
+  seconds = seconds%3600;
+  let minutes= parseInt(seconds/60);
+  seconds = seconds%60;
+  let timeString = "";
+  if(hours){
+    timeString += hours + " hrs ";
+  }
+  if(minutes){
+    timeString += minutes + " min ";
+  }
+  if(seconds){
+    timeString += seconds+ " sec "
+  }
+  if(!compressed){
+    return timeString;
+  }
+  else{
+    if(hours){
+      return(`${hours}h`);
+    }
+    if(minutes){
+      return(`${minutes}m`);
+    }
+    if(seconds){
+      return(`${seconds}s`);
+    }
+  }
+};
+var allKeys, timeSpent, totalTimeSpent,sortedTimeList,topCount,topDataSet,topLabels;
+var color = ["rgba(255, 0, 0, 1)","rgb(255, 51, 0)","rgb(255, 102, 0)","rgb(255, 153, 0)","rgb(255, 204, 0)","rgb(255, 255, 0)","rgb(204, 255, 0)","rgb(153, 255, 0)","rgb(102, 255, 0)","rgb(51, 255, 0)"];
+totalTimeSpent = 0;
 chrome.storage.local.get(null,function(siteList){
   allKeys = Object.keys(siteList);
   timeSpent = [];
-  color=[];
-  insI=document.getElementById('insInside');
-  insB=document.getElementById('insBefore');
+  sortedTimeList = [];
   for (let i = 0; i<allKeys.length;i++ ){
     let webURL = allKeys[i];
     timeSpent.push(siteList[webURL]);
-    color.push('#'+Math.floor(Math.random()*16777215).toString(16));
-    //console.log(webURL);
-    }
-    document.getElementById('btn').addEventListener("click",function(){
+    totalTimeSpent+= siteList[webURL];
+    sortedTimeList.push([webURL,siteList[webURL]]);
+  }
+  sortedTimeList.sort((a,b)=>b[1]-a[1]);
+  console.log(sortedTimeList);
+
+  topCount = allKeys.length>10 ? 10 : allKeys.length;
+  console.log(topCount);
+
+  topDataSet= [];
+  topLabels= [];
+  for(let j=0;j<topCount;j++){
+    topDataSet.push(sortedTimeList[j][1]);
+    topLabels.push(sortedTimeList[j][0]);
+  }
+  
+
+    const webTable = document.getElementById('webList');
     for(let i = 0; i<allKeys.length;i++){
-        let webURL = allKeys[i];
-        let nDA=document.createElement('tr');
-        let nDB=document.createElement('td');
-        let nDC=document.createTextNode(webURL);
-        let nDD=document.createElement('td');
-        let nDE=document.createTextNode(siteList[webURL]+"sec");
-        nDA.appendChild(nDB).appendChild(nDC);
-        nDA.appendChild(nDD).appendChild(nDE);
-        insI.insertBefore(nDA,insB);
-        console.log(webURL)
+        let webURL = sortedTimeList[i][0];
+        let row = document.createElement('tr');
+        let serialNumber = document.createElement('td');
+        serialNumber.innerText = i+1;
+        let siteURL = document.createElement('td');
+        siteURL.innerText= webURL;
+        let siteTime = document.createElement('td');
+        siteTime.innerText = secondsToString(sortedTimeList[i][1]);
+        row.appendChild(serialNumber);
+        row.appendChild(siteURL);
+        row.appendChild(siteTime);
+        webTable.appendChild(row);
+        console.log(row);
     }
-    })
+
+    new Chart(document.getElementById("pie-chart"), {
+      type: 'doughnut',
+      data: {
+        labels: topLabels,
+        datasets: [{
+          label: "Time Spent",
+          backgroundColor: color,
+          data: topDataSet
+        }]
+      },
+      options: {
+        title: {
+          display: true,
+          text: "Top Visited Sites Today"
+        },
+        legend:{
+            display:true
+        },
+        circumference : Math.PI,
+        rotation: Math.PI
+      }
+  });
+
 });
 
-setTimeout(function(){
-  new Chart(document.getElementById("pie-chart"), {
-    type: 'doughnut',
-    data: {
-      labels: allKeys,
-      datasets: [{
-        label: "status",
-        backgroundColor: color,
-        data: timeSpent
-      }]
-    },
-    options: {
-      title: {
-        display: false
-      },
-      legend:{
-          display:false
-      },
-      circumference : Math.PI,
-      rotation: Math.PI
-    }
-});
-},10)
 
 
 
